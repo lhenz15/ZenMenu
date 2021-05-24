738b405f73ed8d15dd7f841498314771b8fac6f7
Luis Henriquez <luiseduardo_henriquez@bhdleon.com.do>
2021-05-23 22:30:36 -0400

    Creating OrderDetailRepository

--- src/main/java/entities/OrderDetail.java
+++ src/main/java/entities/OrderDetail.java
@@ -3,8 +3,31 @@ package entities;
 import java.util.List;
 
 public class OrderDetail {
+    private long id;
     private List<Product> products;
     private List<OrderTax> orderTaxes;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(List<Product> products, List<OrderTax> orderTaxes) {
+        this.products = products;
+        this.orderTaxes = orderTaxes;
+    }
+
+    public OrderDetail(long id, List<Product> products, List<OrderTax> orderTaxes) {
+        this.id = id;
+        this.products = products;
+        this.orderTaxes = orderTaxes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public List<Product> getProducts() {
         return products;
@@ -14,11 +37,11 @@ public class OrderDetail {
         this.products = products;
     }
 
-    public List<OrderTax> getTaxes() {
+    public List<OrderTax> getOrderTaxes() {
         return orderTaxes;
     }
 
-    public void setTaxes(List<OrderTax> orderTaxes) {
+    public void setOrderTaxes(List<OrderTax> orderTaxes) {
         this.orderTaxes = orderTaxes;
     }
 }
