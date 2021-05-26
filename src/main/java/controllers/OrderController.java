db3a1ff6918d3d210bf55b91fb13aeb994e2d416
Luis Henriquez <luiseduardo_henriquez@bhdleon.com.do>
2021-05-25 22:22:08 -0400

    Creating OrderController

---
+++ src/main/java/controllers/OrderController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import entities.Order;
+import repositories.OrderRepository;
+
+import java.util.List;
+
+public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void add(Order order){
+        orderRepository.add(order);
+    }
+
+    public Order update(Order order){
+        return orderRepository.update(order);
+    }
+
+    public Order delete(Long id){
+        return orderRepository.delete(id);
+    }
+
+    public Order find(Long id){
+        return orderRepository.find(id);
+    }
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+}
