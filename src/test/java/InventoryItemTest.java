dcdfcd5ae15c8a1df498681056594aa480125b65
Luis Henriquez <luiseduardo_henriquez@bhdleon.com.do>
2021-06-04 00:29:16 -0400

    Test refactor

--- src/test/java/InventoryItemTest.java
+++ src/test/java/InventoryItemTest.java
@@ -1,7 +1,7 @@
 import controllers.InventoryItemController;
+import controllers.ItemController;
 import entities.Inventory;
 import entities.InventoryItem;
-import entities.Item;
 import exceptions.EntityNotFoundException;
 import utils.IoC;
 
@@ -9,21 +9,21 @@ import java.util.List;
 
 public class InventoryItemTest {
 
-    public static void main(String[] args) {
+    public static void test() {
         IoC ioc = IoC.getInstance();
 
         var inventoryItemController = ioc.<InventoryItemController>getInstance("InventoryItemController");
+        var itemController = ioc.<ItemController>getInstance("ItemController");
 
         Inventory inventory = new Inventory();
 
-        Item chair = new Item("Chair");
-        Item table = new Item("Table");
-        Item waterBottle = new Item("Water Bottle");
+        InventoryItem chairInventoryItem = new InventoryItem(inventory, itemController.find(1L), 10);
+        InventoryItem tableInventoryItem = new InventoryItem(inventory, itemController.find(2L), 5);
+        InventoryItem waterBottleInventoryItem = new InventoryItem(inventory, itemController.find(3L), 50);
 
-        InventoryItem chairInventoryItem = new InventoryItem(inventory, chair, 10);
-        InventoryItem tableInventoryItem = new InventoryItem(inventory, table, 5);
-        InventoryItem waterBottleInventoryItem = new InventoryItem(inventory, waterBottle, 50);
-
+        System.out.print("\n\n\n");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("---------------Testing InventoryItem structure----------------");
         System.out.println("Testing InventoryItemController.add()");
         System.out.println("Adding Chair Inventory Item");
         inventoryItemController.add(chairInventoryItem);
@@ -38,10 +38,10 @@ public class InventoryItemTest {
         System.out.println(inventoryItems.get(1).getQuantity().equals(tableInventoryItem.getQuantity()));
         System.out.println(inventoryItems.get(2).getQuantity().equals(waterBottleInventoryItem.getQuantity()));
 
-        System.out.println("\nTesting ItemController.find() | Finding the Table Item Id = 2");
+        System.out.println("\nTesting InventoryItemController.find() | Finding the Table Item Id = 2");
         System.out.println(inventoryItemController.find(2L).getItem().getName());
 
-        System.out.println("\nTesting ItemController.delete() | Must throw EntityNotFoundException");
+        System.out.println("\nTesting InventoryItemController.delete() | Must throw EntityNotFoundException");
         inventoryItemController.delete(3L);
         try {
             inventoryItemController.find(3L);
@@ -49,7 +49,7 @@ public class InventoryItemTest {
             System.out.println(e.getMessage());
         }
 
-        System.out.println("\nTesting ItemController.update() | Updating Chair Item to Large Chair");
+        System.out.println("\nTesting InventoryItemController.update() | Updating Chair Item to Large Chair");
         InventoryItem inventoryItemToUpdate = inventoryItemController.find(1L);
         System.out.println("Item to update :");
         System.out.println(inventoryItemToUpdate.getItem().getName() + " " + inventoryItemToUpdate.getQuantity());
---
+++ src/test/java/RunTest.java
@@ -0,0 +1,7 @@
+public class RunTest {
+    public static void main(String[] args) {
+        ItemTest.test();
+        InventoryItemTest.test();
+        InventoryTest.test();
+    }
+}
